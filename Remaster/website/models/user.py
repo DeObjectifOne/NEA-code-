79bb77889518ec62241f2712b3127634f79ff0fa
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-12 10:04:14 +0000

    Update user.py

--- Remaster/website/models/user.py
+++ Remaster/website/models/user.py
@@ -5,4 +5,5 @@ from sqlalchemy.sql import func
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullabe=False)
+    email = db.Column(db.String(150), unique=True, nulllable=False)
     password = db.Column(db.string(150), nullable=False)
