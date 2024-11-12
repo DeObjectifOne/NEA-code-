b021caf6069c6f732e9474729ae25c89379c6e45
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-12 08:02:40 +0000

    Add files via upload

---
+++ Remaster/website/models/task.py
@@ -0,0 +1,11 @@
+from utilities import db
+from sqlalchemy.sql import func
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    details = db.Column(db.String(150), nullable=False)
+    due_date = db.Column(db.Date, nullable=True)
+    priority = db.Column(db.Integer, nullable=True)
+    duration = db.Column(db.Integer, nullable=True)
+    difficulty = db.Column(db.Integer, nullable=True)
+    user_id = db.column(db.Integer, db.ForeignKey('user.id'))
---
+++ Remaster/website/models/user.py
@@ -0,0 +1,8 @@
+from utilities import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullabe=False)
+    password = db.Column(db.string(150), nullable=False)
