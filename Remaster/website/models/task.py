20599e15943d244b4de12c32b13714a916e6ad77
DeObjectifOne <ethan.tongson1806@gmail.com>
2024-11-12 10:06:23 +0000

    Update task.py

--- Remaster/website/models/task.py
+++ Remaster/website/models/task.py
@@ -4,6 +4,7 @@ from sqlalchemy.sql import func
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     details = db.Column(db.String(150), nullable=False)
+    creation_date = db.Column(db.DateTime(timezone=True), default=func.now())
     due_date = db.Column(db.Date, nullable=True)
     priority = db.Column(db.Integer, nullable=True)
     duration = db.Column(db.Integer, nullable=True)
